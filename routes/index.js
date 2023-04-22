c5fc404c3782792371415754a07643a008d86c5a
yender <hernandezyender04@gmail.com>
2023-04-22 04:43:47 -0400

    task1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HOLA MUNDO!', nombre:'YENDER', apellido:'OSORIO', cedula:'30626481', seccion:'Seccion3'});
+  res.render('index', { title: "yender osorio"});
 });
 
 module.exports = router;
